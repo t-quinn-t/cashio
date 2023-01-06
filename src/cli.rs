123b00810e6ebc92fb13165d90e43d1fc71a8201
t_quinn_t <qintutao@gmail.com>
2023-01-06 14:27:35 -0800

    [t-quinn-t]dev: added add cli subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,11 @@
-use clap::{Parser, Subcommand};
+use chrono::prelude::*;
+/*
+ * @Author: Quinn Tao @t-quinn-t 
+ * @Date: 2023-01-06 14:01:00 
+ * @Last Modified by: Quinn Tao
+ * @Last Modified time: 2023-01-06 14:02:06
+ */
+use clap::{Parser, Subcommand, Args};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -9,5 +16,22 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    Add(AddCmd)   
+}
 
+/// Add a record to current database
+#[derive(Args)]
+pub struct AddCmd {
+
+    /// Name of the record 
+    name: String,
+
+    /// Amount of the record
+    amount: i32,
+
+    #[arg(short, long)]
+    date: Option<String>,
+
+    #[arg(short, long)]
+    category: Option<String>,
 }
