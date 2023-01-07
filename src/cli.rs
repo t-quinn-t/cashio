2dec33b4740735e05fa1dec4fec7db3dcad3a356
t_quinn_t <qintutao@gmail.com>
2023-01-06 16:10:21 -0800

    [t-quinn-t]dev: added basic cli api

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,8 @@
-use chrono::prelude::*;
 /*
  * @Author: Quinn Tao @t-quinn-t 
- * @Date: 2023-01-06 14:01:00 
+ * @Date: 2023-01-06 15:44:40 
  * @Last Modified by: Quinn Tao
- * @Last Modified time: 2023-01-06 14:02:06
+ * @Last Modified time: 2023-01-06 16:09:30
  */
 use clap::{Parser, Subcommand, Args};
 
@@ -14,13 +13,16 @@ pub struct Cli {
     pub cmd: Commands
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 pub enum Commands {
-    Add(AddCmd)   
+    Add(AddCmd),
+    Ls(ListCmd),
+    Mod(ModCmd),
+    Rm(RmCmd),
 }
 
 /// Add a record to current database
-#[derive(Args)]
+#[derive(Args, Debug)]
 pub struct AddCmd {
 
     /// Name of the record 
@@ -29,9 +31,96 @@ pub struct AddCmd {
     /// Amount of the record
     amount: i32,
 
+    /// Date of this record 
+    /// 
+    /// Supporting format:
+    /// 
+    /// - YYYY-mm-dd OR mm/dd/YYYY
+    /// 
+    /// - mm-dd OR mm/dd, defaulting to current year 
+    /// 
+    /// - dd defaulting to current year/month 
+    date: Option<String>,
+
+    /// The category for this record
+    #[arg(short, long)]
+    category: Option<String>,
+
+    /// Full description of this record
+    #[arg(short, long)]
+    description: Option<String>,
+}
+
+/// List records in current database, default to list records in current month
+#[derive(Args, Debug)]
+pub struct ListCmd {
+    /// Fuzzy find search string, applied to names and descriptions
+    query: Option<String>,
+
+    /// Specify a category
+    #[arg(short, default_value = "default")]
+    category: Option<String>, 
+
+    /// Specify single month 
+    #[arg(short, default_value="current month", conflicts_with="time")]
+    month: Option<String>,
+
+    /// Specify single year 
+    #[arg(short, default_value="current year", conflicts_with="time")]
+    year: Option<String>,
+
+    /// Time range from
+    #[arg(long, group="time")]
+    from: Option<String>,
+
+    /// Time range to
+    #[arg(long, requires="from", group="time")]
+    to: Option<String>,
+}
+
+#[derive(Args, Debug)]
+pub struct ModCmd {
+    /// Id of the record to modify
+    id: i32,
+
+    /// Name of the record 
+    #[arg(short, long)]
+    name: Option<String>,
+
+    /// Amount of the record
+    #[arg(short, long)]
+    amount: Option<i32>,
+
+    /// Date of this record 
+    /// 
+    /// Supporting format:
+    /// 
+    /// - YYYY-mm-dd OR mm/dd/YYYY
+    /// 
+    /// - mm-dd OR mm/dd, defaulting to current year 
+    /// 
+    /// - dd defaulting to current year/month 
     #[arg(short, long)]
     date: Option<String>,
 
+    /// The category for this record
     #[arg(short, long)]
     category: Option<String>,
+
+    /// Full description of this record
+    #[arg(long)]
+    description: Option<String>,
+
+    /// Force apply modification
+    #[arg(short, long)]
+    force: Option<bool>
 }
+
+#[derive(Args, Debug)]
+pub struct RmCmd {
+    /// Id of the record to remove
+    id: i32
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,34 @@
-use cashio::cli::Cli;
+/*
+ * @Author: Quinn Tao @t-quinn-t 
+ * @Date: 2023-01-06 15:50:11 
+ * @Last Modified by: Quinn Tao
+ * @Last Modified time: 2023-01-06 16:10:03
+ */
+
+
+use cashio::cli::{Cli, Commands};
 use clap::Parser;
 
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 fn main() {
+    pretty_env_logger::init();
     let cli = Cli::parse();
+
+    match cli.cmd {
+        Commands::Add(cmd) => {
+            debug!("add command {:?}", cmd);
+        },
+        Commands::Ls(cmd) => {
+            debug!("ls command {:?}", cmd);
+        },
+        Commands::Mod(cmd) => {
+            debug!("mod command {:?}", cmd);
+        },
+        Commands::Rm(cmd) => {
+            debug!("rm command {:?}", cmd);
+        }
+    }
 }
